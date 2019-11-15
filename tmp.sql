427a1174b66806cc7d54813798bf929a663e64f4
Valentyn Troian <34323554+ValentynTroian@users.noreply.github.com>
2019-11-15 16:05:13 +0200

    Create tmp.sql

---
+++ tmp.sql
@@ -0,0 +1,19 @@
+SELECT 
+    c.name AS column_name,
+    i.name AS index_name,
+    c.is_identity
+FROM sys.indexes i
+    inner join sys.index_columns ic  ON i.object_id = ic.object_id AND i.index_id = ic.index_id
+    inner join sys.columns c ON ic.object_id = c.object_id AND c.column_id = ic.column_id
+WHERE i.is_primary_key = 1
+    and i.object_ID = OBJECT_ID('<schema>.<tablename>');
+
+
+SELECT Col.Column_Name from 
+    INFORMATION_SCHEMA.TABLE_CONSTRAINTS Tab, 
+    INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE Col 
+WHERE 
+    Col.Constraint_Name = Tab.Constraint_Name
+    AND Col.Table_Name = Tab.Table_Name
+    AND Constraint_Type = 'PRIMARY KEY'
+    AND Col.Table_Name = '<your table name>'
